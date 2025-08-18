8dfbb8a58d245791c8da6affdf0566b6d82de626
RafkaJulianee <rafkagenggkk@gmail.com>
2025-08-18 12:26:02 +0700

    update

--- function/main.js
+++ function/main.js
@@ -1,6 +1,13 @@
-
+//arrow function
 const nama = (x,y)=>{
     console.log(x,y)
 }
 
 nama(3,3)
+ //membuat angka random
+function random (){
+    console.log(~~(Math.random()*1000))
+}
+random()
+
+
