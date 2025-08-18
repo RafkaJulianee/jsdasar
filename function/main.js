9d4fb9ec1dc7569ae2e507b7e5df6a85241fe52c
RafkaJulianee <rafkagenggkk@gmail.com>
2025-08-18 12:14:07 +0700

    update

---
+++ function/main.js
@@ -0,0 +1,11 @@
+
+function username(nama){
+    return "Username: " + nama;
+}
+alert(username("Benjamin Sesko"));
+function name(firstname,lastname){
+    console.log("First Name: " + firstname);
+    console.log("Last Name: " + lastname);
+}
+
+alert(username("BenjaminSesko"))
